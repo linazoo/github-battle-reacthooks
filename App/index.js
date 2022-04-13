cb9203a85d588bac6eb36306da4e492153d50f5d
Lina Zoo <linazooper@gmail.com>
2022-04-13 15:11:57 -0500

    using the usecontext hook to grab theme off of context

--- App/index.js
+++ App/index.js
@@ -1,10 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
-import Loading from "./components/Loading";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/Loading";
 
 const Popular = React.lazy(() => import("./components/Popular"));
 const Battle = React.lazy(() => import("./components/Battle"));
@@ -21,12 +21,13 @@ function App() {
         <div className={theme}>
           <div className="container">
             <Nav toggleTheme={toggleTheme} />
+
             <React.Suspense fallback={<Loading />}>
               <Switch>
                 <Route exact path="/" component={Popular} />
                 <Route exact path="/battle" component={Battle} />
                 <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>🥴 404</h1>} />
+                <Route render={() => <h1>404</h1>} />
               </Switch>
             </React.Suspense>
           </div>
